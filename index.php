4781d218ac394fbf02de1c8a9b702557e57ea7b3
tervo <tervo@DAT-1610.local>
2017-01-19 12:57:02 +0200

    Added a if- statement

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
 	<title>Dat Bot</title>
 </head>
 <body>
+<?
+php if ($_GET['name'] == NULL):
+?>
 <form>
 	<label for="name">Name</label>
 	<input type="text" name="name" id="name">
 	<input type="submit" value="Send!">
 </form>
+
+<?php endif ?>
+
 </body>
 </html>
