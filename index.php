98a45d6d86cc7dc277b20c7fcf2b57822b81b300
tervo <tervo@DAT-1610.local>
2017-01-19 12:45:08 +0200

    Starting the bot

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dat Bot</title>
+</head>
+<body>
+<form>
+	<label for="name">Name</label>
+	<input type="text" name="name" id="name">
+	<input type="submit" value="Send!">
+</form>
+</body>
+</html>
