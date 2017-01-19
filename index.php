aea06a776565819aec65bea40eead2f83efde9e5
tervo <tervo@DAT-1610.local>
2017-01-19 15:47:46 +0200

    Added name generator

--- index.php
+++ index.php
@@ -14,6 +14,19 @@ php if ($_GET['name'] == NULL):
 	<input type="submit" value="Send!">
 </form>
 
+<?php 
+else: 
+
+$name = $_GET['name'];
+$half_name_length = (int) (mb_strlen($nyttnamn) /2);
+$remaining_chars = mb_strlen($nytt_namn) - $half_name_length;
+$name_end = mb_substr($name, $half_name_length, $remaining_chars);
+$name_beginning = mb_substr($name, 0, $half_name_length);
+$botname = $name_end . $name_beginning;
+?>
+
+<h1><?= $botname ?></h1>
+
 <?php endif ?>
 
 </body>
